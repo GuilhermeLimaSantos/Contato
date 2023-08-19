34172734329fdf4ea4a3bf2c73eb78ddf292809b
Guilherme H. l. dos Santos <125158701+GuilhermeLimaSantos@users.noreply.github.com>
2023-08-19 17:52:06 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
-function submit()
-{
-alert("oi");
-}
+document.getElementById("card").addEventListener("submit", function(event) {
+  var campos = document.querySelectorAll("#card input[type='text'], #card input[type='email']";
+  var formularioValido = true;
+
+  campos.forEach(function(campo) {
+    if (campo.value.trim() === "") {
+      alert("Por favor, preencha todos os campos.");
+      formularioValido = false;
+      event.preventDefault();
+      return;
+    }
+  });
+
+  if (formularioValido) {
+    alert("Formulário enviado com sucesso!");
+  }
+});
